871dbb2adc97507e4e239c73e7007da9bcd95a67
254-Ben <obenard300@gmail.com>
2020-03-12 18:06:14 +0300

    learning reducers and action creators

---
+++ src/playground/redux-100.js
@@ -0,0 +1,105 @@
+import { createStore } from 'redux';
+
+const countReducer = (state = {count: 0}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            // const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+            return {
+                count: state.count + action.incrementBy
+            }
+        case 'DECREMENT':
+            // const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
+            return {
+                count: state.count - action.decrementBy
+            }
+        case 'RESET':
+            return {
+                count: 0
+            }
+        case 'SET':
+            // const count = typeof action.count === 'number' ? action.count : 1
+            return {
+                count: action.count
+            }
+        default:
+            return state;    
+    }
+};
+
+const store = createStore(countReducer);
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+
+// Actions -> an object that gets sent to the store
+
+// Action creater - functions that return action objects
+// const incrementCount = (payload = {}) => {
+//     return {
+//         type: 'INCREMENT',
+//         incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy : 1
+//     }
+// }
+
+// Destructuring incrementCount
+const incrementCount = ({ incrementBy = 1} = {}) => {
+    return {
+        type: 'INCREMENT',
+        incrementBy
+    }
+}
+
+// const decrementCount = (payload = {}) => {
+//     return {
+//         type: 'DECREMENT',
+//         decrementBy: typeof payload.decrementBy === 'number' ? payload.decrementBy : 1
+//     }
+// }
+
+//Destructuring decrementCount
+const decrementCount = ({ decrementBy = 1} = {}) => {
+    return {
+        type: 'DECREMENT',
+        decrementBy
+    }
+}
+
+// const setCount = (payload) => {
+//     return {
+//         type: 'SET',
+//         count: payload.count
+//     }
+// }
+
+// Destructuring setCount
+const setCount = ({ count}) => {
+    return {
+        type: 'SET',
+        count
+    }
+}
+
+const resetCount = () => {
+    return {
+        type: 'RESET'
+    }
+}
+
+
+
+store.dispatch(incrementCount({ incrementBy: 5}));
+
+store.dispatch(incrementCount());
+
+store.dispatch(resetCount());
+
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({ decrementBy: 10}));
+
+store.dispatch(setCount({ count: 150}));
+
+
+
