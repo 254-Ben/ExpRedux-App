19d994dd7faeaa93c4beb8b154ef9bc29f90f2b0
254-Ben <obenard300@gmail.com>
2020-04-02 16:55:27 +0300

    Higher Order Components

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; // High order component
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expense';
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const store = configureStore();
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+store.dispatch(addExpense({ description: 'Water bill',amount: 9500,createdAt: 15000 }));
+store.dispatch(addExpense({ description: 'Gas bill',amount: 6000,createdAt: 27000 }));
+store.dispatch(addExpense({ description: 'Rent',amount: 35700,createdAt: 500 }));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={ store }>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(jsx,document.getElementById('app'));
