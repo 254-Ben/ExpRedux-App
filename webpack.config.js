c03ee6d4afab933f47da62bd7391bafbf9d2de4e
254-Ben <obenard300@gmail.com>
2020-04-30 17:37:22 +0300

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,45 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
+module.exports = (env) => {
+  const isProduction = env === "production";
+  const ExtractCSS = new ExtractTextPlugin("style.css");
+  return {
+    entry: './src/app.js',
+    output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
-  },
-  module: {
+   },
+   module: {
     rules: [{
       loader: 'babel-loader',
       test: /\.js$/,
       exclude: /node_modules/
     }, {
       test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'//managing scss
-      ]
+      use: ExtractCSS.extract({
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            }
+          }
+        ]
+      })
     }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
+   },
+   plugins: [ExtractCSS],
+   devtool: isProduction ? "source-map": 'inline-source-map',
+   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+   },
+  };   
 };
